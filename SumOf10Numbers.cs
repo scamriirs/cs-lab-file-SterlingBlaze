39052b2b068d404fd372b0d02b06a9620c6a6f7a
Aqdas Ahmad Siddiqui <120900589+SterlingBlaze@users.noreply.github.com>
2025-01-22 10:22:05 +0530

    Add files via upload

---
+++ SumOf10Numbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int sum = 0;
+        Console.Write("The first 10 numbers are: ");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            sum += i;
+            Console.Write(i + (i < 10 ? ", " : ""));
+        }
+
+        Console.WriteLine("\nThe sum of the first 10 numbers is: " + sum);
+    }
+}
