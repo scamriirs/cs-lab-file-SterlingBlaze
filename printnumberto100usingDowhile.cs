edf9c74acf22bca46da064a1eafc8f9ea0aa7cbe
Aqdas Ahmad Siddiqui <120900589+SterlingBlaze@users.noreply.github.com>
2025-05-10 23:00:41 +0530

    Create printnumberto100usingDowhile.cs

---
+++ printnumberto100usingDowhile.cs
@@ -0,0 +1,11 @@
+using System;
+
+class PrintNumberTo100usingDOwhile {
+    static void Main() {
+        int i = 1;
+        do {
+            Console.Write(i + " ");
+            i++;
+        } while(i <= 100);
+    }
+}
